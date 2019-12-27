bf7d63e3d15093d242dd66f5b3154f47cb14eb6e
Amit Pitambare <amit.pitambare@cognizant.com>
2019-12-27 10:37:27 +0000

    Contract Validations Added

--- contracts/src/main/java/com/healthcoin/contract/HealthCoinActivityContract.java
+++ contracts/src/main/java/com/healthcoin/contract/HealthCoinActivityContract.java
@@ -1,5 +1,9 @@
 package com.healthcoin.contract;
 
+import java.util.List;
+
+import com.healthcoin.state.HealthCoinActivityState;
+import static net.corda.core.contracts.ContractsDSL.requireThat;
 import net.corda.core.contracts.CommandData;
 import net.corda.core.contracts.Contract;
 import net.corda.core.transactions.LedgerTransaction;
@@ -7,8 +11,15 @@ import net.corda.core.transactions.LedgerTransaction;
 public class HealthCoinActivityContract implements Contract {
 	public static final String ID = "com.healthcoin.contract.HealthCoinActivityContract";
 	@Override
-	public void verify(LedgerTransaction arg0) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
+	public void verify(LedgerTransaction tx) throws IllegalArgumentException {
+		List<HealthCoinActivityState> inpStaes=tx.inputsOfType(HealthCoinActivityState.class);
+		requireThat(require ->{
+			 require.using("There is must be zero input State" , inpStaes.size()<1);
+			 require.using("There is must be one ouput State of type HealthCoinApplication State" , tx.getOutputs().get(0).getData() instanceof HealthCoinActivityState);
+			// require.using("There is must be one ouput State of type HealthCoinApplication State" , tx.getOutputs().get(0).getData() instanceof HealthCoinActivityState);
+			 return require;
+		 });
+		
 
 	}
 	public interface Commands extends CommandData {
